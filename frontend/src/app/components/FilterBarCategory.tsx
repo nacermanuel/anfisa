3c1e886373a2145d03df765f7e0cf1ace8624ef5
Manuel <nacermanuel@gmail.com>
2023-04-19 12:06:37 -0500

    feat(filterBarComponent): Create filter category bar Component

--- frontend/src/app/components/FilterBarCategory.tsx
+++ frontend/src/app/components/FilterBarCategory.tsx
@@ -1,30 +1,16 @@
 import React from 'react'
+import { CardCategory } from './CardCategory'
 
 export const FilterBarCategory = () => {
   return (
     <>
         <div className="carousel carousel-center max-w-fit p-4 space-x-4 rounded-box">
-            <div className="carousel-item">
-                <img src="https://image.clovia.com/media/images/categorybannergeneric/Hair-Care--Mob-Category-Banner-copy-17-01-23.jpg" className="rounded-box w-64" />
-            </div> 
-            <div className="carousel-item">
-                <img src="https://image.clovia.com/media/images/categorybannergeneric/Hair-Care--Mob-Category-Banner-copy-17-01-23.jpg" className="rounded-box w-64" />
-            </div> 
-            <div className="carousel-item">
-                <img src="https://image.clovia.com/media/images/categorybannergeneric/Hair-Care--Mob-Category-Banner-copy-17-01-23.jpg" className="rounded-box w-64" />
-            </div> 
-            <div className="carousel-item">
-                <img src="https://image.clovia.com/media/images/categorybannergeneric/Hair-Care--Mob-Category-Banner-copy-17-01-23.jpg" className="rounded-box w-64" />
-            </div> 
-            <div className="carousel-item">
-                <img src="https://image.clovia.com/media/images/categorybannergeneric/Hair-Care--Mob-Category-Banner-copy-17-01-23.jpg" className="rounded-box w-64" />
-            </div> 
-            <div className="carousel-item">
-                <img src="https://image.clovia.com/media/images/categorybannergeneric/Hair-Care--Mob-Category-Banner-copy-17-01-23.jpg" className="rounded-box w-64" />
-            </div> 
-            <div className="carousel-item">
-                <img src="https://image.clovia.com/media/images/categorybannergeneric/Hair-Care--Mob-Category-Banner-copy-17-01-23.jpg" className="rounded-box w-64" />
-            </div>
+            <CardCategory/>
+            <CardCategory/>
+            <CardCategory/>
+            <CardCategory/>
+            <CardCategory/>
+            <CardCategory/>
         </div>
     </>
   )
