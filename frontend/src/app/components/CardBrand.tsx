9e7fd16cf10b219d2b2a0ec728d19113ff445a4b
Manuel <nacermanuel@gmail.com>
2023-04-19 13:13:14 -0500

    feat(BranFilterBar): add bran filter bar components

---
+++ frontend/src/app/components/CardBrand.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export const CardBrand = () => {
+  return (
+    <div className="card w-fit bg-base-100 shadow-xl">
+        <a href="">
+            <figure><img src="https://moradoapp.vtexassets.com/assets/vtex.file-manager-graphql/images/2c189182-dbad-4778-926b-e341caac952e___6be0e7aac75dcbcbf92bac1a738b1661.svg" alt="Shoes" /></figure>
+            <div className="card-body">
+                <div className="card-actions justify-end">
+                <div className="badge badge-outline">Kuul</div>
+                </div>
+            </div>
+        </a>
+    </div>
+  )
+}
