2978e763bc4569c8bbee67b45c25c1319f6b4696
Luis Alomia <luisfidel44@gmail.com>
2023-04-20 12:49:21 -0500

    feat(SideBar): add componente card sidedar

---
+++ frontend/src/mock/mockcart.ts
@@ -0,0 +1,62 @@
+import { ModelCart } from "@/models/ModelCart";
+
+export const mockcart: ModelCart[] = [
+  {
+    id: 1,
+    name: "paleta miniki tinte color 6-99",
+    price: 200.0,
+    amount: 1,
+    image: "https://moradoapp.vtexassets.com/arquivos/ids/223709-192-auto",
+    category: "cabello",
+    brand: "palet",
+  },
+  {
+    id: 2,
+    name: "paleta miniki tinte color 6-99",
+    price: 200.0,
+    amount: 1,
+    image: "https://moradoapp.vtexassets.com/arquivos/ids/223709-192-auto",
+    category: "cabello",
+    brand: "palet",
+  },
+  {
+    id: 3,
+    name: "sugar body scrub watermelon",
+    price: 120.0,
+    amount: 1,
+    image:
+      "https://moradoapp.vtexassets.com/arquivos/ids/227078-800-auto?v=638131889083530000&width=800&height=auto&aspect=true",
+    category: "cabello",
+    brand: "cent",
+  },
+  {
+    id: 4,
+    name: "sugar body scrub watermelon",
+    price: 120.0,
+    amount: 1,
+    image:
+      "https://moradoapp.vtexassets.com/arquivos/ids/227078-800-auto?v=638131889083530000&width=800&height=auto&aspect=true",
+    category: "cabello",
+    brand: "cent",
+  },
+  /*  {
+    id: 5,
+    name: "sugar body scrub watermelon",
+    price: 120.0,
+    amount: 1,
+    image:
+      "https://moradoapp.vtexassets.com/arquivos/ids/227078-800-auto?v=638131889083530000&width=800&height=auto&aspect=true",
+    category: "cabello",
+    brand: "cent",
+  },
+  {
+    id: 6,
+    name: "sugar body scrub watermelon",
+    price: 120.0,
+    amount: 1,
+    image:
+      "https://moradoapp.vtexassets.com/arquivos/ids/227078-800-auto?v=638131889083530000&width=800&height=auto&aspect=true",
+    category: "cabello",
+    brand: "cent",
+  }, */
+];
