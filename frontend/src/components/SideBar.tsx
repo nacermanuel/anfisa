9203a992876ab9a3a03f736273f4b582aa87d43e
Luis Alomia <luisfidel44@gmail.com>
2023-04-29 13:07:46 -0500

    fix(SideBar-CardSidebar): fix size sideBar car

--- frontend/src/components/SideBar.tsx
+++ frontend/src/components/SideBar.tsx
@@ -7,7 +7,6 @@ import style from "./slideBar.module.css";
 
 import { useState, useEffect } from "react";
 
-
 interface Props {
   setActive: Dispatch<SetStateAction<boolean>>;
   active: boolean;
@@ -15,32 +14,30 @@ interface Props {
 
 const SideBar = ({ setActive, active }: Props) => {
   const [data, setData] = useState<ModelCart[]>([]);
-  const [valores , setValores] = useState({productos:0,total:0})
+  const [valores, setValores] = useState({ productos: 0, total: 0 });
 
   const handleActive = () => {
     setActive(!active);
   };
 
-  useEffect(()=>{
-    let localS = localStorage.getItem('cart')
-    if( localS !== null ){
-      let carrito = JSON.parse(localS)
-      setData(carrito)
+  useEffect(() => {
+    let localS = localStorage.getItem("cart");
+    if (localS !== null) {
+      let carrito = JSON.parse(localS);
+      setData(carrito);
     }
-    
-  },[])  
+  }, []);
 
-  const deleteProduct = (id:number) => {
-    let localS = localStorage.getItem('cart')
-    if( localS !== null ){
-      let carrito = JSON.parse(localS)
-      let nuevo= [...carrito.filter((e:ModelCart) => e.id !== id)]
+  const deleteProduct = (id: number) => {
+    let localS = localStorage.getItem("cart");
+    if (localS !== null) {
+      let carrito = JSON.parse(localS);
+      let nuevo = [...carrito.filter((e: ModelCart) => e.id !== id)];
       localStorage.clear();
-      localStorage.setItem('cart', JSON.stringify(nuevo))
-      setData(nuevo)
+      localStorage.setItem("cart", JSON.stringify(nuevo));
+      setData(nuevo);
     }
-  }
-
+  };
 
   return (
     <div
@@ -49,7 +46,7 @@ const SideBar = ({ setActive, active }: Props) => {
       }`}
     >
       <div className="w-full bg-stone-600 opacity-95"></div>
-      <div className="flex flex-col justify-center items-center w-[24rem] max-[500px]:w-[21rem] h-screen bg-white">
+      <div className="flex flex-col justify-center items-center w-[24rem] max-[500px]:w-[22rem] h-screen bg-white">
         <div className="flex p-2 w-full items-center bg-white">
           <button
             className="w-[2rem] h-[2rem] rounded-full bg-pink-400 hover:bg-pink-300 transition duration-300 flex justify-center items-center shadow-md"
@@ -64,20 +61,34 @@ const SideBar = ({ setActive, active }: Props) => {
 
         <ul className={`h-full p-1 ${style.contenedorScroll} my-1`}>
           {data.map((cart: ModelCart) => (
-            <CardSidebar key={cart.id} data={cart} deleteProduct={deleteProduct} />
+            <CardSidebar
+              key={cart.id}
+              data={cart}
+              deleteProduct={deleteProduct}
+            />
           ))}
         </ul>
 
         <div className="flex flex-col justify-center gap-1 px-4 py-4 text-sm w-full max-[500px]:w-full h-[9rem] bg-pink-200">
           <div className="flex justify-between">
             <p className="text-gray-500 font-semibold">Total productos:</p>
-            <p className="text-lg text-gray-600 font-semibold">
-              {data.length}
-            </p>
+            <p className="text-lg text-gray-600 font-semibold">{data.length}</p>
           </div>
           <div className="flex justify-between">
             <p className="text-gray-500 font-semibold">Total a pagar:</p>
-            <p className="text-xl font-bold text-pink-400">$ { data ? data.reduce((a,b: ModelCart) => a + ( typeof b.price === 'string' ?  parseInt(b.price) * b.cartAmount : b.price * b.cartAmount ),0) : 0}</p>
+            <p className="text-xl font-bold text-pink-400">
+              ${" "}
+              {data
+                ? data.reduce(
+                    (a, b: ModelCart) =>
+                      a +
+                      (typeof b.price === "string"
+                        ? parseInt(b.price) * b.cartAmount
+                        : b.price * b.cartAmount),
+                    0
+                  )
+                : 0}
+            </p>
           </div>
           <button className="btn">Pagar</button>
         </div>
