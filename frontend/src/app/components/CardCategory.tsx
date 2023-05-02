a7f736b53e4e4af2ca34f364116fc83ecea61f88
Manuel <nacermanuel@gmail.com>
2023-05-01 23:43:40 -0500

    feat(sliderCategory):Eliminate text

--- frontend/src/app/components/CardCategory.tsx
+++ frontend/src/app/components/CardCategory.tsx
@@ -9,7 +9,6 @@ export const CardCategory = ( {data} : Props) => {
   return (
         <div className="carousel-item">
             <a href={`/categories/${data.name}`}>
-                <p>{data.name}</p>
                 <img src={data.image} alt={data.name} className="rounded-box w-64"/>
             </a>
         </div>
